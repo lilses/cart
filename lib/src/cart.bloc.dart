40a1e931b051ae10e1a566db62387da93b39868f
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-02-10 19:22:16 +0000

    feat: use router from blocs

--- lib/src/cart.bloc.dart
+++ lib/src/cart.bloc.dart
@@ -1,30 +1,34 @@
 import 'dart:async';
 
 import 'package:list_component/list_component.dart';
+import 'package:nav/nav.dart';
 import 'package:product/product.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stripe/stripe.dart';
 import 'package:transferwise/transferwise.dart';
-
+import 'package:router/router.dart';
 import 'cart.repo.dart';
 import 'cart.state.dart';
 
 class CartBloc extends Cubit<CartState> {
   final CartRepo cartRepo;
-
-  late final StreamSubscription cartRepoSubscription;
   final StripeRepo stripeRepo;
   final TransferwiseRepo transferwiseRepo;
+  final RouterRepo routerRepo;
+  RouterState? _routerState;
+
+  late final StreamSubscription cartRepoSubscription;
+  late final StreamSubscription routerRepoSubscription;
 
   CartBloc({
     required this.cartRepo,
     required this.stripeRepo,
     required this.transferwiseRepo,
-  }) : super(const CartState.some([],[])){
+    required this.routerRepo,
+  }) : super(const CartState.some([], [])) {
     subscribe();
   }
-
 
   @override
   Future<void> close() {
@@ -34,30 +38,35 @@ class CartBloc extends Cubit<CartState> {
 
   void subscribe() {
     cartRepoSubscription = cartRepo.items.listen(
-          (event){
-        emit(CartState.some(event.products,event.listItems));
+      (event) {
+        emit(CartState.some(event.products, event.listItems));
+      },
+      onError: (error) => print("STREAM ERROR: $error"),
+    );
+    routerRepoSubscription = routerRepo.items.listen(
+      (event) {
+        _routerState = event;
       },
       onError: (error) => print("STREAM ERROR: $error"),
     );
   }
 
-  void retrigger(){
+  void retrigger() {
     final a = state.products;
     final b = state.listItems;
-    emit(const CartState.some([],[]));
-    emit(CartState.some(a,b));
+    emit(const CartState.some([], []));
+    emit(CartState.some(a, b));
   }
 
   List<ProductEnum> get cart => state.products;
 
-
   addToCart(ProductEnum product) {
     final productList = state.products.toList();
     productList.add(product);
-    final listItems = productList.map((e) => e.toListItemsForCart(
-        '£${productList.totalPrice().toStringAsFixed(2)}'
-    ))
-    .toList();
+    final listItems = productList
+        .map((e) => e.toListItemsForCart(
+            '£${productList.totalPrice().toStringAsFixed(2)}'))
+        .toList();
     final stripeListItem = stripeRepo.toListItem();
     final transferwiseListItem = transferwiseRepo.toListItem();
     listItems.add(stripeListItem);
@@ -65,12 +74,23 @@ class CartBloc extends Cubit<CartState> {
     cartRepo.addToCart(productList, listItems);
   }
 
-  // List<ListItem> paymentListItems(){
-  //   final List<ListItem> list = [];
-  //   final stripeListItem = stripeRepo.toListItem();
-  //   final transferwiseListItem = transferwiseRepo.toListItem();
-  //   list.add(stripeListItem);
-  //   list.add(transferwiseListItem);
-  //   return list;
-  // }
+  navigateToStripe() {
+    if (_routerState != null) {
+      _routerState!.mapOrNull(
+          some: (some) => routerRepo.changeRoute(RouterState.some(
+              some.uri.replace(
+                path: NavEnum.stripe.path.replaceAll("/", ""),
+              ),
+              NavEnum.stripe)));
+    }
+  }
+
+// List<ListItem> paymentListItems(){
+//   final List<ListItem> list = [];
+//   final stripeListItem = stripeRepo.toListItem();
+//   final transferwiseListItem = transferwiseRepo.toListItem();
+//   list.add(stripeListItem);
+//   list.add(transferwiseListItem);
+//   return list;
+// }
 }
